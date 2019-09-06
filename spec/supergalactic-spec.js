6a0857ce25c23e50677334796bff1c43fab7ff8a
Chris Burge <topher.burge@gmail.com>
2019-09-06 09:27:26 -0700

    write two more specs to calculate venus years and mars years.

--- spec/supergalactic-spec.js
+++ spec/supergalactic-spec.js
@@ -1,8 +1,22 @@
-import { mercYears } from '../src/galactic.js';
+import { mercYears, venYears, marYears } from '../src/galactic.js';
 
 describe('mercury years', function() {
   it('should take a users age and return it in mercury years', function() {
     let earthYear = 20;
     expect(mercYears(earthYear)).toEqual(83);
+  })
+})
+
+describe('venus years', function() {
+  it('should take a users age and return it in venus years', function() {
+    let earthYear = 20;
+    expect(venYears(earthYear)).toEqual(32);
+  })
+})
+
+describe('mars years', function() {
+  it('should take a users age and return it in venus years', function() {
+    let earthYear = 20;
+    expect(marYears(earthYear)).toEqual(10);
   })
 })
--- src/galactic.js
+++ src/galactic.js
@@ -3,3 +3,15 @@ export function mercYears(earthYear){
   mercuryYears = Math.floor(earthYear / .24);
   return mercuryYears;
 }
+
+export function venYears(earthYear){
+  let venusYears = 0;
+  venusYears = Math.floor(earthYear / .62);
+  return venusYears;
+}
+
+export function marYears(earthYear){
+  let marsYears = 0;
+  marsYears = Math.floor(earthYear / 1.88);
+  return marsYears;
+}
