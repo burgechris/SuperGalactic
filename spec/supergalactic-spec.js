479a61dc889697a7ce4b98cba313bba7b2ffc91d
Chris Burge <topher.burge@gmail.com>
2019-09-06 10:45:25 -0700

    rewrite the spec to add a beforeEach before specs.

--- spec/supergalactic-spec.js
+++ spec/supergalactic-spec.js
@@ -1,22 +1,27 @@
-import { mercYears, venYears, marYears } from '../src/galactic.js';
+import { Human } from '../src/galactic.js';
 
-describe('mercury years', function() {
+describe('Human', function() {
+  let humanAge;
+
+  beforeEach(function() {
+    humanAge = new Human(38,78);
+  });
+
   it('should take a users age and return it in mercury years', function() {
-    let earthYear = 20;
-    expect(mercYears(earthYear)).toEqual(83);
+    expect(age.mercYears()).toEqual(158);
   })
 })
 
-describe('venus years', function() {
-  it('should take a users age and return it in venus years', function() {
-    let earthYear = 20;
-    expect(venYears(earthYear)).toEqual(32);
-  })
-})
-
-describe('mars years', function() {
-  it('should take a users age and return it in venus years', function() {
-    let earthYear = 20;
-    expect(marYears(earthYear)).toEqual(10);
-  })
-})
+// describe('venus years', function() {
+//   it('should take a users age and return it in venus years', function() {
+//     let earthYear = 20;
+//     expect(venYears(earthYear)).toEqual(32);
+//   })
+// })
+//
+// describe('mars years', function() {
+//   it('should take a users age and return it in venus years', function() {
+//     let earthYear = 20;
+//     expect(marYears(earthYear)).toEqual(10);
+//   })
+// })
--- src/galactic.js
+++ src/galactic.js
@@ -1,26 +1,22 @@
-class Human {
-  constructor(name, age, gender) {
-  this.name = name;
+export class Human {
+  constructor(age, gender) {
   this.age = age;
   this.gender = gender;
  }
+
+ mercYears() {
+   return Math.floor(this.age / .24);
+ }
 }
 
-
-export function mercYears(earthYear){
-  let mercuryYears = 0;
-  mercuryYears = Math.floor(earthYear / .24);
-  return mercuryYears;
-}
-
-export function venYears(earthYear){
-  let venusYears = 0;
-  venusYears = Math.floor(earthYear / .62);
-  return venusYears;
-}
-
-export function marYears(earthYear){
-  let marsYears = 0;
-  marsYears = Math.floor(earthYear / 1.88);
-  return marsYears;
-}
+// export function venYears(earthYear){
+//   let venusYears = 0;
+//   venusYears = Math.floor(earthYear / .62);
+//   return venusYears;
+// }
+//
+// export function marYears(earthYear){
+//   let marsYears = 0;
+//   marsYears = Math.floor(earthYear / 1.88);
+//   return marsYears;
+// }
